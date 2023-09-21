bca11348e2290d6da315d9c27db5de5c7f1b683a
Gordon McCreight <gordon@mccreight.com>
2023-09-21 09:15:22 -0700

    Add a script that runs isql

--- unixodbc.rb
+++ unixodbc.rb
@@ -1,7 +1,6 @@
 class Unixodbc < Formula
   desc "ODBC 3 connectivity for UNIX"
   homepage "https://www.unixodbc.org/"
-  # url "https://www.unixodbc.org/unixODBC-2.3.11.tar.gz"
   url "ftp://ftp.unixodbc.org/pub/unixODBC/unixODBC-2.3.13pre.tar.gz"
   license "LGPL-2.1-or-later"
 
@@ -38,13 +37,13 @@ end
 # class Unixodbc < Formula
 #   desc "ODBC 3 connectivity for UNIX"
 #   homepage "https://www.unixodbc.org/"
-#   url "https://www.unixodbc.org/unixODBC-2.3.11.tar.gz"
+#   url "ftp://ftp.unixodbc.org/pub/unixODBC/unixODBC-2.3.13pre.tar.gz"
 #   mirror "https://fossies.org/linux/privat/unixODBC-2.3.11.tar.gz"
 #   sha256 "d9e55c8e7118347e3c66c87338856dad1516b490fb7c756c1562a2c267c73b5c"
 #   license "LGPL-2.1-or-later"
 # 
 #   livecheck do
-#     url "https://www.unixodbc.org/download.html"
+#     url "ftp://ftp.unixodbc.org/pub/unixODBC/unixODBC-2.3.13pre.tar.gz"
 #     regex(/href=.*?unixODBC[._-]v?(\d+(?:\.\d+)+)\.t/i)
 #   end
 # 
